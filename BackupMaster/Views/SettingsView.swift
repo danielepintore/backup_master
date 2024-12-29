3f2355f2f7d1e108227d2948a315b82511de22e9
Daniele Pintore <daniele.pintore1@gmail.com>
2024-12-29 02:55:01 +0100

    Settings view has now a title and remove unused view

--- BackupMaster/Views/SettingsView.swift
+++ BackupMaster/Views/SettingsView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct SettingsView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .navigationTitle("Settings")
+            .navigationBarTitleDisplayMode(.inline)
     }
 }
 
